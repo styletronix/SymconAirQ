0ca32cf4f090bda8af99f308569a365ee3fb49ca
Andreas W. Pross <andreas.pross@styletronix.net>
2023-07-10 21:33:14 +0200

    update

--- Air Q/module.php
+++ Air Q/module.php
@@ -16,29 +16,33 @@ class AirQ extends IPSModule
 		$this->RegisterPropertyInteger("refreshAverage", 20);
 		$this->RegisterPropertyString('Sensors', '');
 
-		$this->RegisterVariableInteger('timestamp', 'Zeitpunkt der Messung');
-		$this->RegisterVariableString('DeviceID', 'DeviceID');
-		$this->RegisterVariableFloat('health', 'Gesundheit');
-		$this->RegisterVariableFloat('performance', 'Leistungsfähigkeit');
-		$this->RegisterVariableFloat('virus', 'Virusfrei-Index');
-		$this->RegisterVariableFloat('co2', 'Kohlendioxid (CO2)');
-		$this->RegisterVariableFloat('co', 'Kohlenmonixod (CO)');
-		$this->RegisterVariableFloat('o3', 'Ozon (O3)');
-		$this->RegisterVariableFloat('pm1', 'Feinstaub (PM1)');
-		$this->RegisterVariableFloat('pm2_5', 'Feinstaub (PM2.5)');
-		$this->RegisterVariableFloat('pm10', 'Feinstaub (PM10)');
-		$this->RegisterVariableInteger('TypPS', 'Feinstaub Partikelgröße');
-		$this->RegisterVariableFloat('oxygen', 'Sauerstoff (O2)');
-		$this->RegisterVariableFloat('h2s', 'Schwefelwasserstoff (H2S)');
-		$this->RegisterVariableFloat('no2', 'Stickstoffdioxid (NO2)');
-		$this->RegisterVariableFloat('tvoc', 'VOC');
-		$this->RegisterVariableFloat('temperature', 'Temperatur');
-		$this->RegisterVariableFloat('sound', 'Lautstärke');
-		$this->RegisterVariableFloat('sound_max', 'Lautstärke (max)');
-		$this->RegisterVariableFloat('pressure', 'Luftdruck');
-		$this->RegisterVariableFloat('humidity', 'Luftfeuchtigkeit (relativ)');
-		$this->RegisterVariableFloat('humidity', 'Luftfeuchtigkeit (absolut)');
-		$this->RegisterVariableFloat('dewpt', 'Taupunkt');
+		$this->RegisterVariableInteger('timestamp', $this->Translate('Measure Time'));
+		$this->RegisterVariableString('DeviceID', $this->Translate('DeviceID'));
+
+		// TODO: Create Profiles
+		// TODO: Internal list of Captions for Sensors in form.json
+
+		// $this->RegisterVariableFloat('health', $this->Translate('Health');
+		// $this->RegisterVariableFloat('performance', $this->Translate('Performance');
+		// $this->RegisterVariableFloat('virus', $this->Translate('Virusfrei-Index');
+		// $this->RegisterVariableFloat('co2', $this->Translate('Kohlendioxid (CO2)');
+		// $this->RegisterVariableFloat('co', $this->Translate('Kohlenmonixod (CO)');
+		// $this->RegisterVariableFloat('o3', $this->Translate('Ozon (O3)');
+		// $this->RegisterVariableFloat('pm1', $this->Translate('Feinstaub (PM1)');
+		// $this->RegisterVariableFloat('pm2_5', $this->Translate('Feinstaub (PM2.5)');
+		// $this->RegisterVariableFloat('pm10', $this->Translate('Feinstaub (PM10)');
+		// $this->RegisterVariableInteger('TypPS', $this->Translate('Feinstaub Partikelgröße');
+		// $this->RegisterVariableFloat('oxygen', $this->Translate('Sauerstoff (O2)');
+		// $this->RegisterVariableFloat('h2s', $this->Translate('Schwefelwasserstoff (H2S)');
+		// $this->RegisterVariableFloat('no2', $this->Translate('Stickstoffdioxid (NO2)');
+		// $this->RegisterVariableFloat('tvoc', $this->Translate('VOC');
+		// $this->RegisterVariableFloat('temperature', '$this->Translate(Temperatur');
+		// $this->RegisterVariableFloat('sound', 'Lautstärke');
+		// $this->RegisterVariableFloat('sound_max', 'Lautstärke (max)');
+		// $this->RegisterVariableFloat('pressure', 'Luftdruck');
+		// $this->RegisterVariableFloat('humidity', 'Luftfeuchtigkeit (relativ)');
+		// $this->RegisterVariableFloat('humidity', 'Luftfeuchtigkeit (absolut)');
+		// $this->RegisterVariableFloat('dewpt', 'Taupunkt');
 
 		$this->RegisterTimer("update", ($this->ReadPropertyBoolean('active') ? $this->ReadPropertyInteger('refresh') * 1000 : 0), 'IPS_RequestAction($_IPS["TARGET"], "TimerCallback", "update");');
 		$this->RegisterTimer("updateAverage", ($this->ReadPropertyBoolean('active') ? $this->ReadPropertyInteger('refreshAverage') * 1000 : 0), 'IPS_RequestAction($_IPS["TARGET"], "TimerCallback", "updateAverage");');
@@ -192,52 +196,54 @@ class AirQ extends IPSModule
 
 		$this->SetTimerInterval('update', ($this->ReadPropertyBoolean('active') ? $this->ReadPropertyInteger('refresh') * 1000 : 0));
 		$this->SetTimerInterval('updateAverage', ($this->ReadPropertyBoolean('active') ? $this->ReadPropertyInteger('refreshAverage') * 1000 : 0));
-		$this->Update();
+
+		$this->AddOrUpdateVariables();
+		$this->Update(true);
 	}
-	private function parseData($data)
-	{
-		foreach ($data as $key => $value) {
-			$valID = @$this->GetIDForIdent($key);
-			if (!$valID) {
-				continue;
-			}
+	// private function parseData($data)
+	// {
+	// 	foreach ($data as $key => $value) {
+	// 		$valID = @$this->GetIDForIdent($key);
+	// 		if (!$valID) {
+	// 			continue;
+	// 		}
 
-			if (is_array($value)) {
-				SetValue($valID, $value[0]);
+	// 		if (is_array($value)) {
+	// 			SetValue($valID, $value[0]);
 
-				for ($i = 1; $i < count($value); $i++) {
-					$indent = 'value_' . $i;
-					$val2ID = @IPS_GetObjectIDByIdent($indent, $valID);
-					if ($val2ID) {
-						SetValue($val2ID, $value[$i]);
-					}
-				}
-			} else {
-				switch ($key) {
-					case 'timestamp':
-					case 'measuretime':
-						SetValue($valID, $value / 1000);
-						break;
+	// 			for ($i = 1; $i < count($value); $i++) {
+	// 				$indent = 'value_' . $i;
+	// 				$val2ID = @IPS_GetObjectIDByIdent($indent, $valID);
+	// 				if ($val2ID) {
+	// 					SetValue($val2ID, $value[$i]);
+	// 				}
+	// 			}
+	// 		} else {
+	// 			switch ($key) {
+	// 				case 'timestamp':
+	// 				case 'measuretime':
+	// 					SetValue($valID, $value / 1000);
+	// 					break;
 
-					case 'performance':
-					case 'health':
-						SetValue($valID, $value / 10);
-						break;
+	// 				case 'performance':
+	// 				case 'health':
+	// 					SetValue($valID, $value / 10);
+	// 					break;
 
-					default:
-						SetValue($valID, $value);
-				}
-			}
-		}
-	}
-	public function Update()
-	{
+	// 				default:
+	// 					SetValue($valID, $value);
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	public function GetDataDecoded(){
 		$pw = $this->ReadPropertyString('password');
 		$url = trim($this->ReadPropertyString('url'), '\\') . '/data';
 
 		if (!$pw || !$url) {
 			$this->SetStatus(204);
-			return;
+			return null;
 		}
 
 		try {
@@ -245,20 +251,20 @@ class AirQ extends IPSModule
 			$this->SendDebug("getDataFromUrl", $json, 0);
 		} catch (Exception $ex) {
 			$this->SetStatus(201);
-			return;
+			return null;
 		}
 
 		try {
 			$data = json_decode($json, true);
 			if (!$data || !$data['content']) {
 				$this->SetStatus(202);
-				return;
+				return null;
 			}
 			$this->SendDebug("json_decode", $data['content'], 0);
 
 		} catch (Exception $ex) {
 			$this->SetStatus(202);
-			return;
+			return null;
 		}
 
 		try {
@@ -266,99 +272,91 @@ class AirQ extends IPSModule
 			$this->SendDebug("decryptString", $data, 0);
 			if (!$data) {
 				$this->SetStatus(203);
-				return;
+				return null;
 			}
-			$data = json_decode($data, true);
+			return json_decode($data, true);
 		} catch (Exception $ex) {
 			$this->SetStatus(203);
-			return;
+			return null;
 		}
 
+	}
+	public function Update($includeAggregated = false)
+	{
+		$data = $this->GetDataDecoded;
+		if ($data){
+			$this->WriteValues($data, $includeAggregated);
 
-		$this->parseData($data);
-
-		$this->SetStatus(102);
+			$this->SetStatus(102);
+		}
 	}
 
-	public function CheckLimits()
+	public function WriteValues($data, $includeAggregated = false)
 	{
 		$sensorlist = json_decode($this->ReadPropertyString("Sensors"));
 		$newSeverity = [];
+
 		for ($x = 0; $y < count($sensorlist); $x++) {
 			$sensor = $sensorlist[$x];
-			$SensorValueID = @$this->GetIDForIdent($sensor('Sensor'));
 			$indentSensorStatus = $sensor['Sensor'] . '_status';
-			$SensorStatusID = @$this->GetIDForIdent($indentSensorStatus);
+			$SensorStatusID = $this->RegisterVariableInteger($indentSensorStatus, $sensor['FriendlyName'] . ' - ' . $this->Translate('Status'));
+
+			if (!array_key_exists($indentSensorStatus, $newSeverity)) {
+				$newSeverity[$indentSensorStatus] = 0;
+			}
+
+			if (array_key_exists($sensor['Sensor'], $data)) {
+				$currentValue = ($data[$sensor['Sensor']] + $sensor['Offset']) * $sensor['Multiplicator'];
+				$SensorValueID = $this->RegisterVariableFloat($sensor['Sensor'], $sensor['FriendlyName']);
+				SetValue($SensorValueID, $currentValue);
+			}
 
 			for ($y = 0; $y < count($sensor['Limits']); $y++) {
 				$limit = $sensor['Limits'][$y];
-
+				
 				if ($limit['Timespan'] == 0) {
 					$variableID = $SensorValueID;
 					$statusID = $SensorStatusID;
-					$indentStatus = $indentSensorStatus;
-					if (!$statusID) {
-						$statusID = IPS_CreateVariable(1);
-						IPS_SetParent($statusID, $this->InstanceID);
-						IPS_SetIdent($statusID, $indentStatus);
-						IPS_SetName($statusID, $sensor['FriendlyName'] . ' - Status');
+
+					if (
+						($limit['UpperLimit'] != 0 && $currentValue > $limit['UpperLimit']) ||
+						($limit['LowerLimit'] != 0 && $currentValue < $limit['LowerLimit'])
+					) {
+						if ($limit['Severity'] > $newSeverity[$indentSensorStatus]) {
+							$newSeverity[$indentSensorStatus] = $limit['Severity'];
+						}
 					}
 
-				} else {
-					$indentValue = $sensor['Sensor'] . '_' . $limit['Timespan'];
-					$indentStatus = $sensor['Sensor'] . '_' . $limit['Timespan'] . '_status';
+				} elseif ($includeAggregated) {
+					 $indentValue = $sensor['Sensor'] . '_' . $limit['Timespan'];
+					 $indentStatus = $sensor['Sensor'] . '_' . $limit['Timespan'] . '_status';
+					 $variableID = $this->RegisterVariableFloat( $indentValue, $sensor['FriendlyName'] . ' (' . $limit['Timespan'] . ')');
 					
-					$variableID = @$this->GetIDForIdent($indentValue);
-					
-					if (!$variableID) {
-						$variableID = IPS_CreateVariable(2);
-						IPS_SetParent($variableID, $this->InstanceID);
-						IPS_SetIdent($variableID, $indentValue);
-						IPS_SetName($variableID, $sensor['FriendlyName'] . ' (' . $limit['Timespan'] . ')');
+					 if (!array_key_exists($indentStatus, $newSeverity)) {
+						$newSeverity[$indentStatus] = 0;
 					}
 
-					if (!$statusID) {
-						$statusID = IPS_CreateVariable(1);
-						IPS_SetParent($statusID, $this->InstanceID);
-						IPS_SetIdent($statusID, $indentStatus);
-						IPS_SetName($statusID, $sensor['FriendlyName'] . ' (' . $limit['Timespan'] . ' - Status)');
-					}
-				}
+					 $t = time();
+					$rolingAverage = $this->GetAggregatedFloatingValue($variableID, $t - ($limit['Timespan'] * 60), $t);
+					 
 
-				if (!array_key_exists($indentStatus, $newSeverity)){
-					$newSeverity[$indentStatus] = 0;
-				}
+					$value = $rolingAverage['Avg'];
+					SetValue($variableID, $value);
 
-				$value = GetValue($variableID);
-
-				if (($limit['UpperLimit'] != 0 && $value > $limit['UpperLimit']) ||
-					($limit['LowerLimit'] != 0 && $value < $limit['LowerLimit']) 
-				) {
-					if (!$newSeverity[$indentStatus] >= $limit['Severity']){
-						$newSeverity[$indentStatus] = $limit['Severity'];
+					if (
+					($limit['UpperLimit'] != 0 && $value > $limit['UpperLimit']) ||
+					($limit['LowerLimit'] != 0 && $value < $limit['LowerLimit'])
+					 ) {
+					 	if (!$newSeverity[$indentStatus] >= $limit['Severity']) {
+					 		$newSeverity[$indentStatus] = $limit['Severity'];
+					 	}
 					}
 				}
 			}
 		}
 
-		foreach ($newSeverity as $key => $val){
-			$statusID = $this->GetIDForIdent($key);
-			if ($statusID){
-				SetValue($statusID, $val);
-			}
-		}
-	}
-
-	public function UpdateAverage()
-	{
-		$sensorlist = json_decode($this->ReadPropertyString("Sensors"));
-
-		foreach ($sensorlist as $sensor) {
-			foreach ($sensor['Limits'] as $limit) {
-				if ($limit['Timespan'] == 0) {
-
-				}
-			}
+		foreach ($newSeverity as $key => $val) {
+			SetValue($$this->GetIDForIdent($key), $val);
 		}
 	}
 
@@ -373,6 +371,106 @@ class AirQ extends IPSModule
 		$data = curl_exec($ch);
 		curl_close($ch);
 		return $data;
+	}
+
+	private function GetAggregatedFloatingValue( $varId, $start, $end, $archiveControlID = null){
+		if (!$archiveControlID){
+			$archiveControlID = IPS_GetInstanceListByModuleID('{43192F0B-135B-4CE7-A0A7-1475603F3060}')[0];
+		}
+
+		$endtime = $end;
+		$avgs = [];
+		$diff = $end - $start;
+
+		#### Get minutes to previous full hour
+		$date = getdate($end);
+		$fullHr = mktime($date['hours'], 00, 00, $date['mon'], $date['mday'], $date['year']);
+		$diffToFit = $end - $fullHr;
+		if ($diffToFit > $diff) {
+			$diffToFit = $diff;
+		}
+		$diffToFit = floor($diffToFit / 60) * 60;
+
+		if ($diffToFit > 0) {
+			print('DIFF ' . $diffToFit . "\n");
+			$werte = AC_GetAggregatedValues($archiveControlID, $varId, 6, $end - $diffToFit, $end - 1, 0);
+			$avgs = array_merge($avgs, $werte);
+			$end = $fullHr;
+			$diff = $end - $start;
+		}
+
+
+		# Get hours to previous midnight
+		if ($diff > 0) {
+			$date = getdate($end);
+			$fullDay = mktime(00, 00, 00, $date['mon'], $date['mday'], $date['year']);
+			$diffToFit = $end - $fullDay;
+			if ($diffToFit > $diff) {
+				$diffToFit = $diff;
+			}
+			$diffToFit = floor($diffToFit / 3600) * 3600;
+
+			if ($diffToFit >= 3600) {
+				print('DIFF ' . $diffToFit . "\n");
+
+				$werte = AC_GetAggregatedValues($archiveControlID, $varId, 0, $end - $diffToFit, $end - 1, 0);
+				$avgs = array_merge($avgs, $werte);
+				$diff = $diff - $diffToFit;
+			}
+			$end = $start + $diff;
+		}
+
+		### Now in Steps of Days Hours and Minutes
+		while ($diff > 0) {
+			if ($diff >= 86400) {
+				## Full Days
+				$diffToFit = floor($diff / 86400) * 86400;
+				$level = 1;
+			} elseif ($diff >= 3600) {
+				## Full Hours
+				$level = 0;
+				$diffToFit = floor($diff / 3600) * 3600;
+			} elseif ($diff >= 60) {
+				## Remaining as Minutes
+				$level = 6;
+				$diffToFit = floor($diff / 60) * 60;
+				;
+			} else {
+				## Ignore everything below 1 minute
+				break;
+			}
+
+			$end = $start + $diff;
+			$werte = AC_GetAggregatedValues($archiveControlID, $varId, $level, $end - $diffToFit, $end - 1, 0);
+			$avgs = array_merge($avgs, $werte);
+
+			$diff = $diff - $diffToFit;
+		}
+
+		$avgSum = 0;
+		$avgCount = 0;
+		$max = 0;
+		$min = INF;
+
+		foreach ($avgs as $avg) {
+			$avgSum = $avgSum + ($avg['Avg'] * $avg['Duration'] / 60);
+			$avgCount = $avgCount + ($avg['Duration'] / 60);
+			if ($avg['Max'] > $max) {
+				$max = $avg['Max'];
+			}
+			if ($avg['Min'] < $min) {
+				$min = $avg['Min'];
+			}
+		}
+
+		return [
+			"Duration" => $avgCount * 60,
+			"Avg" => $avgSum / $avgCount,
+			"Max" => $max,
+			"Min" => $min,
+			"DurationDifference" => ($avgCount * 60) - ($endtime - $start) ,
+			"Avgs"  => $avgs
+		];
 	}
 
 	private function decryptString($data, $password)
@@ -402,7 +500,7 @@ class AirQ extends IPSModule
 				break;
 
 			case "updateAverage":
-				$this->UpdateAverage();
+				$this->Update(true);
 				break;
 
 			default:
